11ec4e8a4525b91ae86b93f46910085d300f074a
IsidroMtz <78511514+IsidroMtz@users.noreply.github.com>
2024-07-27 08:30:15 -0600

    Primer Commit

---
+++ Julio24/src/main/java/javaTest/Selenium_test.java
@@ -0,0 +1,17 @@
+package javaTest;
+import java.util.Scanner;
+
+// Pedir un número e indicar si es positivo o negativo
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_test {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
